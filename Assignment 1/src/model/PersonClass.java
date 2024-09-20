5369a5c17806a2f8db94e221a696fa904546af21
guna031 <vudatala.s@northeastern.edu>
2024-09-19 21:26:13 -0400

    assignment 1 has been added

---
+++ Assignment 1/src/model/PersonClass.java
@@ -0,0 +1,204 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author gunav
+ */
+public class PersonClass {
+    
+    String frstName;
+    String lstName;
+    String email;
+    String phoneNumber;
+    String driverLicenseNumber;
+    String driverLicenseState;
+    String socialSecurityNumber;
+    String addressLine1;
+    String addressLine2;
+    String city;
+    String state;
+    String zip;
+    String telephoneNumber;
+    String faxNumber;
+    String profession;
+    String citizenship;
+    String linkedInUserName;
+    String organization;
+    String githubUserName;
+    String leetCodeUserName; 
+    String highestDegreeAchieved;
+   
+
+    public String getFrstName() {
+        return frstName;
+    }
+
+    public void setFrstName(String frstName) {
+        this.frstName = frstName;
+    }
+
+    public String getLstName() {
+        return lstName;
+    }
+
+    public void setLstName(String lstName) {
+        this.lstName = lstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getDriverLicenseNumber() {
+        return driverLicenseNumber;
+    }
+
+    public void setDriverLicenseNumber(String driverLicenseNumber) {
+        this.driverLicenseNumber = driverLicenseNumber;
+    }
+
+    public String getDriverLicenseState() {
+        return driverLicenseState;
+    }
+
+    public void setDriverLicenseState(String driverLicenseState) {
+        this.driverLicenseState = driverLicenseState;
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public void setSocialSecurityNumber(String socialSecurityNumber) {
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public String getFaxNumber() {
+        return faxNumber;
+    }
+
+    public void setFaxNumber(String faxNumber) {
+        this.faxNumber = faxNumber;
+    }
+    
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public String getCitizenship() {
+        return citizenship;
+    }
+
+    public void setCitizenship(String citizenship) {
+        this.citizenship = citizenship;
+    }
+
+    public String getLinkedInUserName() {
+        return linkedInUserName;
+    }
+
+    public void setLinkedInUserName(String linkedInUserName) {
+        this.linkedInUserName = linkedInUserName;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    public String getGithubUserName() {
+        return githubUserName;
+    }
+
+    public void setGithubUserName(String githubUserName) {
+        this.githubUserName = githubUserName;
+    }
+
+    public String getLeetCodeUserName() {
+        return leetCodeUserName;
+    }
+
+    public void setLeetCodeUserName(String leetCodeUserName) {
+        this.leetCodeUserName = leetCodeUserName;
+    } 
+    
+     public String getHighestDegreeAchieved() {
+        return highestDegreeAchieved;
+    }
+
+    public void setHighestDegreeAchieved(String highestDegreeAchieved) {
+        this.highestDegreeAchieved = highestDegreeAchieved;
+    }
+   
+}
